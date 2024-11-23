e9d582a183bbc26866676a263b23ccc09175f38a
Matheus Filipe <math.filipe23@gmail.com>
2024-11-22 23:09:05 -0300

    [feat] Create_Horista_and_FolhaPGTO

---
+++ atividade-6/src/FolhaPGTO.java
@@ -0,0 +1,35 @@
+public class FolhaPGTO {
+
+    public static void main(String[] args) {
+        Mensalista men1, men2;
+        Horista hora1, hora2;
+
+        // Empregados mensalistas
+        men1 = new Mensalista("Jose", "Rua abc", "Junior");
+        men1.calcularSalario();
+        men1.calcularInss();
+        System.out.println("Nome: " + men1.getNome());
+        System.out.println("EnderecoNome: " + men1.getEndereco());
+        System.out.println("Salário: " + men1.getSalario());
+
+        men2 = new Mensalista("Ana", "Rua sem fim", "Senior");
+        men2.calcularSalario();
+        men2.calcularInss();
+        System.out.println("Nome: " + men2.getNome());
+        System.out.println("EnderecoNome: " + men2.getEndereco());
+        System.out.println("Salário: " + men2.getSalario());
+
+        // Empregados horistas
+        hora1 = new Horista("Carlos", "Rua xyz",20);
+        hora1.calcularSalario();
+        System.out.println("Nome: " + hora.getNome());
+        System.out.println("EnderecoNome: " + hora1.getEndereco());
+        System.out.println("Salário: " + hora1.getSalario());
+
+        hora2 = new Horista("Carlos", "Rua xyz",20);
+        hora2.calcularSalario();
+        System.out.println("Nome: " + hora2.getNome());
+        System.out.println("EnderecoNome: " + hora2.getEndereco());
+        System.out.println("Salário: " + hora2.getSalario());
+    }
+}
